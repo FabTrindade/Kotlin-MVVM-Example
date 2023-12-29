08cd6922b4a64e5b6e0a1d2e28531e548a65f0f9
Fabricio Trindade <fabrtrindade@gmail.com>
2023-12-29 09:57:56 -0300

    Creating a viewModel

--- app/src/main/java/com/fabscorp/kotlinmvvmexample/MainActivity.kt
+++ app/src/main/java/com/fabscorp/kotlinmvvmexample/MainActivity.kt
@@ -2,14 +2,20 @@ package com.fabscorp.kotlinmvvmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.fabscorp.kotlinmvvmexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Ensures that the view model is instantiated correctly following the MainActivity lifecycle
+        // So, if MainActivity is destroyed, the viewModel will be as well.
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/fabscorp/kotlinmvvmexample/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.fabscorp.kotlinmvvmexample
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
