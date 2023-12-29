df4a439d6e21c23d53f2e13c69d8093d0974354d
Fabricio Trindade <fabrtrindade@gmail.com>
2023-12-29 10:21:43 -0300

    An example of using the viewModel

--- app/src/main/java/com/fabscorp/kotlinmvvmexample/MainActivity.kt
+++ app/src/main/java/com/fabscorp/kotlinmvvmexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fabscorp.kotlinmvvmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.fabscorp.kotlinmvvmexample.databinding.ActivityMainBinding
 
@@ -17,5 +18,14 @@ class MainActivity : AppCompatActivity() {
         //Ensures that the view model is instantiated correctly following the MainActivity lifecycle
         // So, if MainActivity is destroyed, the viewModel will be as well.
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        setObserver()
+    }
+    private fun setObserver() {
+        //It stay observing to that, any change that occurs in viewModel.welcome,
+        // the binding.textWelcome.text attribute is updated.
+        viewModel.welcome().observe(this, Observer {
+            binding.textWelcome.text = it
+        })
     }
 }
--- app/src/main/java/com/fabscorp/kotlinmvvmexample/MainViewModel.kt
+++ app/src/main/java/com/fabscorp/kotlinmvvmexample/MainViewModel.kt
@@ -1,6 +1,17 @@
 package com.fabscorp.kotlinmvvmexample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+
+    private var textWelcome = MutableLiveData<String>()
+
+    init {
+        textWelcome.value = "Olá!!!"
+    }
+    fun welcome(): LiveData<String> {
+        return textWelcome
+    }
 }
